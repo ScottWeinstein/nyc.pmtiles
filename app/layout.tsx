c29c21d2efd16d747b218e9a743a08637bbbf2c5
Scott Weinstein <scottwww@squidnet.com>
2024-01-27 09:54:05 -0500

    using react-leaflet

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" async></script>
-        <script src="https://unpkg.com/leaflet-hash@0.2.1/leaflet-hash.js" async></script>
-        <script src="https://unpkg.com/protomaps-leaflet@1.24.2/dist/protomaps-leaflet.min.js" async></script>
-      </head>
+      <head></head>
       <body>{children}</body>
     </html>
   );
